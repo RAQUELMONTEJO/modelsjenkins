a8e1f6649d983f975c91f4a0667039facc6a49c7
raque <raque@host.docker.internal>
2024-12-16 15:45:35 +0100

    modificacion

--- models/src/test/java/com/arquitecturajava/webspring/test/PersonaTest.java
+++ models/src/test/java/com/arquitecturajava/webspring/test/PersonaTest.java
@@ -17,5 +17,45 @@ class PersonaTest {
 		boolean esMayor = p.esMayorEdad();
 		assertTrue(esMayor);
 	}
+	
+	@Test
+	void mayorEdadtest2() {
+		//AAA
+		
+		//arrange preparar
+		Persona p = new Persona("pepe", 20);
+		boolean esMayor = p.esMayorEdad();
+		assertTrue(esMayor);
+	}
+	
+	@Test
+	void mayorEdadtest3() {
+		//AAA
+		
+		//arrange preparar
+		Persona p = new Persona("pepe", 20);
+		boolean esMayor = p.esMayorEdad();
+		assertTrue(esMayor);
+	}
+	
+	@Test
+	void mayorEdadtest4() {
+		//AAA
+		
+		//arrange preparar
+		Persona p = new Persona("pepe", 20);
+		boolean esMayor = p.esMayorEdad();
+		assertTrue(esMayor);
+	}
+	
+	@Test
+	void mayorEdadtest5() {
+		//AAA
+		
+		//arrange preparar
+		Persona p = new Persona("pepe", 10);
+		boolean esMayor = p.esMayorEdad();
+		assertTrue(esMayor);
+	}
 
 }
